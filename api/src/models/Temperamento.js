76b64d0d002243cb18ea7d1a2b55877ff46fc7e4
Julio Kenneth Andersson Mazuelos Vargas <kennethmazuelosvargas.km@gmail.com>
2022-03-29 19:22:49 -0500

    Faltan el post de agregar mascotas en los controllers

--- api/src/models/Temperamento.js
+++ api/src/models/Temperamento.js
@@ -6,11 +6,7 @@ module.exports = (sequelize) => {
   sequelize.define(
     "temperamento",
     {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
+      
       name: {
         type: DataTypes.STRING,
         allowNull: false,
